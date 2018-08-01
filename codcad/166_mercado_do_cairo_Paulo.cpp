a1803f310c15d0884b965679033ffec6c57d5aa7
Paulo Miranda <paulomirandamss@hotmail.com>
2018-07-31 21:01:17 -0300

    modified:   codcad/166_mercado_do_cairo_Paulo.cpp

--- codcad/166_mercado_do_cairo_Paulo.cpp
+++ codcad/166_mercado_do_cairo_Paulo.cpp
@@ -3,55 +3,28 @@
 
 using namespace std;
 
-int t[4*MAXN][4*MAXN];
+int t[MAXN][MAXN];
 
-int sum_y (int vx, int vy, int tly, int try_, int ly, int ry) {
-	if (ly > ry)
-		return 0;
-	if (ly == tly && try_ == ry)
-		return t[vx][vy];
-	int tmy = (tly + try_)>>1;
-	return max(sum_y (vx, (vy<<1), tly, tmy, ly, min(ry,tmy))
-		,sum_y (vx, (vy<<1)+1, tmy+1, try_, max(ly,tmy+1), ry));
-}
- 
-int sum_x (int vx, int tlx, int trx, int lx, int rx, int ly, int ry) {
-	if (lx > rx)
-		return 0;
-	if (lx == tlx && trx == rx)
-		return sum_y (vx, 1, 0, 999, ly, ry);
-	int tmx = (tlx + trx)>>1;
-	return max(sum_x ((vx<<1), tlx, tmx, lx, min(rx,tmx), ly, ry)
-		,sum_x ((vx<<1)+1, tmx+1, trx, max(lx,tmx+1), rx, ly, ry));
+int maximo(int x, int y){
+	int m = 0;
+	
+	for(int a=x; a>0; a-= a&-a){
+		for(int b=y; b>0; b-=b&-b){
+			m = max(t[a][b], m);
+		}
+	}
+	return m;
 }
 
-void update_y (int vx, int lx, int rx, int vy, int ly, int ry, int x, int y, int new_val) {
-	if (ly == ry) {
-		if (lx == rx)
-			t[vx][vy] = new_val;
-		else
-			t[vx][vy] = max(t[(vx<<1)][vy], t[(vx<<1)+1][vy]);
+void atualizar(int x, int y, int v){
+	for(int a=x; a<MAXN; a+= a&-a){
+		for(int b=y; b<MAXN; b+=b&-b){
+			t[a][b] = max(v, t[a][b]) ;
+		}
 	}
-	else {
-		int my = (ly + ry)>>1;
-		if (y <= my)
-			update_y (vx, lx, rx, (vy<<1), ly, my, x, y, new_val);
-		else
-			update_y (vx, lx, rx, (vy<<1)+1, my+1, ry, x, y, new_val);
-		t[vx][vy] = max(t[vx][(vy<<1)], t[vx][(vy<<1)+1]);
-	}
+	
 }
- 
-void update_x (int vx, int lx, int rx, int x, int y, int new_val) {
-	if (lx != rx) {
-		int mx = (lx + rx)>>1;
-		if (x <= mx)
-			update_x ((vx<<1), lx, mx, x, y, new_val);
-		else
-			update_x ((vx<<1)+1, mx+1, rx, x, y, new_val);
-	}
-	update_y (vx, lx, rx, 1, 0, 999, x, y, new_val);
-} 
+
 int main(){	
 	
 	ios::sync_with_stdio(0);
@@ -62,22 +35,21 @@ int main(){
 	
 	while(test--){
 		
-		int maximo = 0;
+		int maxi = 0;
 		cin >> n;
 		
 		for(int i=0; i<n; i++){
 			int a, b;
 			cin >> a >> b;
-			a--;
-			b--;
-			int m = sum_x(1, 0, 999, 0, a, 0, b) + 1;			
-			update_x(1, 0, 999, a, b, m);
+
+			int m = maximo(a, b) + 1;			
+			atualizar(a, b, m);
 			
-			maximo = max(maximo, m);
+			maxi = max(maxi, m);
 			
 		}
 		
-		cout << maximo << endl;
+		cout << maxi << endl;
 		
 		memset(t, 0, sizeof(t));
 	}
