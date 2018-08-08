d6b2bd034acac0de102c0bd8a308d8ba0f0cbbe6
dougnobrega <dougnobrega22@gmail.com>
2018-08-08 09:46:07 -0300

    Head.cpp

---
+++ HEAD.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+							// Typedef's  start //
+typedef long long   ll;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii > vpi;
+
+							//	Typedef's end //
+							//	Define's start //
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i < (int)(b); i++)
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+#define all(x) x.begin(),x.end()
+#define MOD 1000000007
+#define endl '\n'
+
+							// Define's end //
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	return 0;
+}
+
