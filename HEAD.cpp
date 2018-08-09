2fef31811d28897be4c27c6fc30565cba980ae5c
dougnobrega <dougnobrega22@gmail.com>
2018-08-09 10:54:45 -0300

    Update

--- HEAD.cpp
+++ HEAD.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// Prioridade
 typedef long long   ll;
 typedef pair<int,int> pii;
 typedef vector<int> vi;
@@ -12,36 +13,15 @@ typedef vector<pii> vpi;
 #define S second
 #define PB push_back
 #define MP make_pair
-#define REP(i,a,b) for(int i = a; i < (int)(b); i++)
+#define REP(i,a,b) for(int i = a; i < (b); i++)
 #define INF 0x3f3f3f3f
 #define INFLL 0x3f3f3f3f3f3f3f3f
 #define all(x) x.begin(),x.end()
 #define MOD 1000000007
 #define endl '\n'
-
 #define mdc(a, b) (__gcd((a), (b)))
 #define mmc(a, b) (((a)*(b))/__gcd((a), (b)))
-
-#define cinc(a) scanf(" %c", &a)
-#define cinll(a) scanf("%lld", &a)
-
-#define cini(a) scanf("%ld", &a)
-#define cin2i(a, b) scanf("%ld %ld", &a, &b)
-#define cin3i(a, b, c) scanf("%ld %ld %ld", &a, &b, &c)
-#define cin4i(a, b, c, d) scanf("%ld %ld %ld %ld", &a, &b, &c, &d)
-#define cin5i(a, b, c, d, e) scanf("%ld %ld %ld %ld %ld", &a, &b, &c, &d, &e)
-
-#define coutc(a) printf("%c", a)
-#define coutcl(a) printf("%c\n", a)
-#define coutll(a) printf("%lld", a)
-#define coutlll(a) printf("%lld\n", a)
-
-#define couti(a) printf("%ld", a)
-#define coutil(a) printf("%ld\n", a)
-#define cout2i(a, b) printf("%ld %ld\n", a, b)
-#define cout3i(a, b, c) printf("%ld %ld %ld\n", a, b, c)
-#define cout4i(a, b, c, d) printf("%ld %ld %ld %ld\n", a, b, c, d)
-#define cout5i(a, b, c, d, e) printf("%ld %ld %ld %ld %ld\n", a, b, c, d, e)
+// fim da Prioridade
 
 int main() {
 	ios::sync_with_stdio(0);
